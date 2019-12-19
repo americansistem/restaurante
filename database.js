e3e8755c8c708b4e2161856d2a79c7cd36b1b825
americansistem <56614751+americansistem@users.noreply.github.com>
2019-12-19 12:28:37 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb://primerUsuario:ed25797059@cluster0-shard-00-00-dsvka.mongodb.net:27017,cluster0-shard-00-01-dsvka.mongodb.net:27017,cluster0-shard-00-02-dsvka.mongodb.net:27017/ananke-examen?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uri = "mongodb://sisfer:Bunker1234@cluster0-shard-00-00-fosgi.mongodb.net:27017,cluster0-shard-00-01-fosgi.mongodb.net:27017,cluster0-shard-00-02-fosgi.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
     .then(db => { console.log('Conexión exitosa con cluster MongoDB: Atlas') })
